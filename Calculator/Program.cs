2459863cd1c9014febdda1b66dde45ce43342845
siddesh bagare <siddeshbagare@gmail.com>
2021-11-30 23:04:53 +0530

    last try

---
+++ Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CalculatorFunctions;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions obj = new Functions();
+
+            Console.WriteLine("Enter two numbers");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(string.Format("Addition = {0}", obj.Addition(num1, num2)));
+
+            Console.WriteLine(string.Format("Subtraction = {}" , obj.Subtraction(num1, num2)));
+
+            Console.WriteLine(string.Format("Multiplication = {}" , obj.Multiplication(num1, num2)));
+
+            Console.WriteLine(string.Format("Division = {}" , obj.Division(num1, num2)));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CalculatorFunctions/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace CalculatorFunctions
+{
+    //functions of calculator
+    public class Functions
+    {
+        //Addition
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
